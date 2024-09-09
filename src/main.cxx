3931952fc1666ac42c407d02ab5493542527aad3
Daniel McIntosh <McIntosh.Daniel.k@gmail.com>
2024-09-08 22:40:50 -0500

    Done, Comented

--- src/main.cxx
+++ src/main.cxx
@@ -25,23 +25,35 @@ int main(int argc, char**argv){
 	double t = std::stod(argv[3]);
 	int n = std::stoi(argv[4]);
 
+
+	// THIS SECTION JUST GENERATES n POINTS IN A HELIX 
+	// IT DOES THIS BY INITIALIZING SOME POINT AT POINT (0,0,0)
+	// THEN CREATES A SECOND AT A DISTANCE OF d FROM THE ORIGIN ON THE X AXIS
+	// THEN IT CREATES A THIRD AT A DISTANCE OF d FROM THE SECOND WITH AN ANGLE OF a
+	// THEN ADDS A FOURTH TAKING INTO ACCOUNT THE TORTIONAL ANGLE
+	// IT THEN TRANSLATES ALL THE POINTS SO THAT x2->x1, x3->x2, x4->x3, AND x1 IS PUSHED BACK
+	// THEN ANOTHER POINT IS ADDED IN THE SAME SPOT AS THE FOURTH POINT AND THE PATTERN CONTINUES
 	double ia =3.141592-a;
 
 	Matrix I=Matrix::Identity(4,4);
+	//TRANSLATION MATRIX
 	Matrix T{{1.0,0.0,0.0,d},
 		{0.0,1.0,0.0,0.0},
 		{0.0,0.0,1.0,0.0},
 		{0.0,0.0,0.0,1.0}};
+	//a ROATION MATRIX
 	Matrix A{{std::cos(ia),-std::sin(ia),0.0,0.0},
 		{std::sin(ia),std::cos(ia),0.0,0.0},
 		{0.0,0.0,1.0,0.0},
 		{0.0,0.0,0.0,1.0}};
+	//t TORSIONAL ROTATION MATRIX
 	Matrix B{{1.0,0.0,0.0,0.0},
 		{0.0,std::cos(t),-std::sin(t),0.0},
 		{0.0,std::sin(t),std::cos(t),0.0},
 		{0.0,0.0,0.0,1.0}};
 
-
+	//NOTE THE POINTS USE 4 COORDINATES TO ALLOW FOR TRANSLATION MATRICIES TO BE USED
+	//THE FORUTH COORDINATE WILL ALWAYS BE 1
 	std::cout << d << " " << a << " " << t << " " << n << std::endl;
 	Matrix points(4,n);
 	points.col(0)=Matrix{{0.0},{0.0},{0.0},{1.0}};
@@ -56,41 +68,7 @@ int main(int argc, char**argv){
 		points.col(i)=refpoint;
 	}
 
-	for(int i = 0; i < n; i++){
-		points.col(i)=Matrix{{2.5*std::cos(i*3.14159/12)},{2.5*std::sin(i*3.14159/12)},{i*3.0},{1.0}};
-	}
-
-	std::cout << "POINTS\n";
-	for(int i = 0; i < n; i++){
-		std::cout << points.col(i).transpose() << std::endl;
-	}
-	
-	std::cout << "DIFFS\n";
-	std::vector<Matrix> diff; diff.resize(n-1);
-	for(int i = 0; i < n-1;i++){
-		diff[i]=points.col(i+1)-points.col(i);
-		std::cout << diff[i].transpose() <<std::endl;
-	}
-	std::cout << "Distances:\n";
-	for(int i = 0; i < n-1; i++){
-		double v = (diff[i].transpose()*diff[i])(0,0);
-		std::cout << v/d << std::endl;
-	}	
-	std::cout << "Angles:\n";
-	for(int i = 0; i < n-2; i++){
-		//std::cout << -diff[i].transpose()*diff[i+1] << std::endl;
-		double v = (-diff[i].transpose()*diff[i+1])(0,0);
-		std::cout << std::acos(v/(d*d)) << std::endl;
-	}
-	std::cout << "Dihedrals:\n";
-	for(int i = 0; i < n-3; i++){
-		Matrix norm1=cross(-diff[0],diff[1]),
-		       norm2=cross(-diff[1],diff[2]);
-		double length= d*d*std::sin(a);
-		double torsion=(norm1.transpose()*norm2)(0,0);
-		std::cout << std::acos(torsion/(length*length)) << std::endl;
-	}
-
+	//CALCULATE THE CENTER OF MASS AND TRANSLATE COORDINATES SO IT IS THE ORIGIN
 	Matrix sum=Matrix::Zero(4,1);
 	for(int i = 0; i < n; i++) sum+=points.col(i);
 	sum/=(double)n;
@@ -102,8 +80,8 @@ int main(int argc, char**argv){
 		cmpoints(1,i)=points(1,i)-sum(1,0);
 		cmpoints(2,i)=points(2,i)-sum(2,0);
 	}
-	points=Matrix(0,0);
 
+	//CALCULATE THE INTERTIA TENSOR
 	Matrix inertiaTensor=Matrix::Identity(3,3);
 	for(int i = 0; i < 3; i++){
 		for(int j = 0; j < 3; j++){
@@ -115,43 +93,44 @@ int main(int argc, char**argv){
 			}
 		}
 	}
-
 	std::cout <<"INERTIA TENSOR:\n"<< inertiaTensor << std::endl;
 
+	//DIAGONALIZE THE INERTIA TENSOR
 	Eigen::SelfAdjointEigenSolver<Matrix> eigen(inertiaTensor);
 	std::cout << "THE EIGENVALUES ARE:\n" << eigen.eigenvalues() << std::endl;
 	std::cout << "THE EIGENVECTORS ARE:\n" << eigen.eigenvectors() << std::endl;
 
 	Matrix evecs=eigen.eigenvectors();
 
+	//SWAP THE EIGENVECTORS SO LOWEST EIGENVALUE WILL ALLIGN WITH Z AXIS
 	int smallest=0;
 	for(int i = 1; i< 3;i++){
-		if(eigen.eigenvalues()(i) < eigen.eigenvalues()(0)) smallest=i;
+		if(evecs(i) < evecs(0)) smallest=i;
 	}
-	Matrix smallestvec=evecs.col(smallest);
-	std::cout << "SMALLEST VEC\n";
-	std::cout << smallestvec.transpose() << std::endl;
-
-	double angle=std::atan2(smallestvec(1,0),smallestvec(0,0));
-	Matrix zrot{{std::cos(-angle),-std::sin(-angle),0.0},
-		    {std::sin(-angle), std::cos(-angle),0.0},
-		    {0.0,	      0.0,            1.0}};
-	smallestvec=zrot*smallestvec;
-	std::cout << smallestvec.transpose() << std::endl;
-	angle=std::atan2(smallestvec(0,0),smallestvec(2,0));
-	Matrix yrot{{std::cos(-angle), 0.0,std::sin(-angle)},
-		    {0.0,	       1.0,              0.0},
-		    {-std::sin(-angle),0.0,std::cos(-angle)}};
-	smallestvec=yrot*smallestvec;
-	std::cout << smallestvec.transpose() << std::endl;
-
+	if(smallest ==0){
+		Matrix ev1=evecs.col(1), ev2=evecs.col(2);
+		evecs.col(2)=evecs.col(0);
+		evecs.col(1)=ev2;
+		evecs.col(0)=ev1;
+	}
+	if(smallest==1){
+		Matrix ev0=evecs.col(0), ev2=evecs.col(2);
+		evecs.col(2)=evecs.col(1);
+		evecs.col(0)=ev2;
+		evecs.col(1)=ev0;
+	}
+	
+	//OUTPUTS THE XYZ FILE OF THE UNTRANSFORMED COORDINATES
 	std::ofstream file1("output1.xyz");
 	file1 << n << std::endl << "XYZ file made by HelixCalc" << std::endl;
 	for(int i = 0; i < n; i++){
 		file1 << "H " << cmpoints(0,i) << " " << cmpoints(1,i) << " " << cmpoints(2,i)<<std::endl;
 	}
-	cmpoints=yrot*zrot*cmpoints;
 	
+	//TRANSLATES COORDINATES TO EIGENBASIS
+	cmpoints=evecs.transpose()*cmpoints;
+
+	//OUTPUTS TRANSLATED XYZ FILE
 	std::ofstream file2("output2.xyz");
 	file2 << n << std::endl << "XYZ file made by HelixCalc" << std::endl;
 	for(int i = 0; i < n; i++){
